6e64c4abaf6462b96cb1c73f4396f7eca2932f10
EMANUELMED <emedrano@iwa.com.mx>
2026-05-13 19:01:50 -0600

    Mis ejercicios listos para enviar

--- src/main/java/ClasesyObjetos/Mascota.java
+++ src/main/java/ClasesyObjetos/Mascota.java
@@ -1,4 +1,21 @@
 package ClasesyObjetos;
 
 public class Mascota {
+
+    String Especie;
+    int edad;
+    double peso;
+
+    //Contructor, retirna la misma clase y recibe parametros para inicializar los atributos
+    public Mascota(String Especie, int edad, double peso) {
+        this.Especie = Especie;
+        this.edad = edad;
+        this.peso = peso;
+    }
+
+
+    public String imprimeDatos(){
+        return "La especie de este animal  es: " + Especie +" su edad es: " + edad + " años"+ " y pesa: " + peso + " kilos";
+    }
+
 }
--- src/main/java/practica_de_ejercicios/ejerciciosjava/VariablesYConstantes.java
+++ src/main/java/practica_de_ejercicios/ejerciciosjava/VariablesYConstantes.java
@@ -1,4 +1,21 @@
 package practica_de_ejercicios.ejerciciosjava;
 
+import java.util.ArrayList;
+
 public class VariablesYConstantes {
+    // Defino el método
+    static void saludar() {
+        System.out.println("¡Hola! ¿Cómo estás?");
+    }
+
+    // Lo uso / llamo
+    public static void main(String[] args) {
+        saludar(); // Aquí se ejecuta lo que hay dentro
+    }
+
+
 }
+
+
+
+
