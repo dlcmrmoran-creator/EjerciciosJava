85640f40b0946559e1bb43d2f4dd34f3234e6c18
EMANUELMED <emedrano@iwa.com.mx>
2026-05-13 18:50:52 -0600

    se agregaron

--- src/main/java/ClasesyObjetos/Automovil.java
+++ src/main/java/ClasesyObjetos/Automovil.java
@@ -1,4 +1,42 @@
 package ClasesyObjetos;
 
 public class Automovil {
+    String marca;
+String color;
+int cilindraje;
+
+
+    //Contructor, retirna la misma clase y recibe parametros para inicializar los atributos
+    public Automovil(String marca, String color, int cilindraje){
+        this.marca = marca;
+        this.color = color;
+        this.cilindraje = cilindraje;
+    }
+
+    //Contructor, retirna la misma clase y recibe parametros para inicializar los atributos
+    public Automovil(int cilindraje){
+        this.cilindraje = cilindraje;
+    }
+
+    public String imprimeDatosVehiculo(){
+        return "La marca de este automovil es: " + marca + " con color " +  color +  " y cilindraje: " + cilindraje;
+    }
+
+
+
+    public int getCilindraje(){
+        return cilindraje;
+    }
+
+
+    public String imprimeMarca(){
+        return marca;
+    }
+
+
+public String imprimeColor(){
+        return color;
 }
+
+}
+
--- src/main/java/ClasesyObjetos/Motocicleta.java
+++ src/main/java/ClasesyObjetos/Motocicleta.java
@@ -1,4 +1,22 @@
 package ClasesyObjetos;
 
 public class Motocicleta {
+
+    String marca;
+    String color;
+    int modelo;
+
+    //Contructor, retirna la misma clase y recibe parametros para inicializar los atributos
+    public Motocicleta(String marca, String color, int modelo){
+        this.marca = marca;
+        this.color = color;
+        this.modelo= modelo;
+    }
+
+
+    public String imprimeDatosMotocicleta(){
+        return "La marca de esta motocicleta es: " + marca + " con color " +  color +  " y es modelo: " + modelo;
+    }
+
 }
+
